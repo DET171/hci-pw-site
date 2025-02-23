04fc317e76307a7aab60e584f13b269d2e39a02d
Canaris <mrgeek484@gmail.com>
2025-02-23 14:01:27 +0800

    feat(frontend): add utility functions, global styles, and button component; remove unused favicon and tailwind.css (and initial iter of landing page)

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -7,7 +7,7 @@ import {
 	ScrollRestoration,
 } from '@remix-run/react';
 
-import './tailwind.css';
+import './styles/global.css';
 
 export const links: LinksFunction = () => [
 	{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -18,16 +18,16 @@ export const links: LinksFunction = () => [
 	},
 	{
 		rel: 'stylesheet',
-		href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+		href: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Corinthia:wght@400;700&display=swap',
 	},
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang='en'>
 			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta charSet='utf-8' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<Meta />
 				<Links />
 			</head>
---
+++ apps/frontend/app/routes/projects.tsx
@@ -0,0 +1,13 @@
+import type { MetaFunction } from '@remix-run/react';
+
+export const meta: MetaFunction = () => {
+	return [
+		{
+			title: 'Projects',
+		},
+		{
+			name: 'description',
+			content: 'Welcome to the HCI PW collections site!',
+		},
+	];
+};
